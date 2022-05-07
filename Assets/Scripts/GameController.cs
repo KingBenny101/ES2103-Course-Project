2966f2ba14c47754ddbb24b2769a579c1e71bb3c
KingBenny101 <benstin2727@gmail.com>
2022-05-07 11:18:38 +0530

    Update GameController.cs

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -238,9 +238,9 @@ public class GameController : MonoBehaviour
 
         string n = "\n";
         scoreDisplay.GetComponent<UnityEngine.UI.Text>().text =
-            $@"YOUR SCORE{n}{this.grid.SCORE.TOTAL_SCORE}";
+            $@"YOUR SCORE{n}{Mathf.Ceil(this.grid.SCORE.TOTAL_SCORE)}";
         tableDisplay.GetComponent<UnityEngine.UI.Text>().text =
-            $@"Money spent{n}INR {this.grid.SCORE.COST_THRESHOLD - this.grid.SCORE.TOTAL_COST}{n}{n}LAND Occupied{n}{this.grid.SCORE.TOTAL_AREA} Acre{n}{n}Emission Rate{n}{this.grid.SCORE.TOTAL_EMISSION} KG/3.6S";
+            $@"Money spent{n}INR {Mathf.Ceil(this.grid.SCORE.COST_THRESHOLD - this.grid.SCORE.TOTAL_COST)} Cr{n}{n}LAND Occupied{n}{Mathf.Ceil(this.grid.SCORE.TOTAL_AREA)} Acre{n}{n}Emission Rate{n}{Mathf.Ceil(this.grid.SCORE.TOTAL_EMISSION)} KG/3.6S";
     }
 
     public void CreateNewPowerPlant(int choice)
